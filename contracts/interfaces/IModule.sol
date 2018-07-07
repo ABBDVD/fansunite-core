0ab52a03f592bf004785b013a64ce96497c06143
Manan Mehta <mehtamanan@icloud.com>
2018-07-06 20:22:48 -0700

    add introspections/ERC165.sol, interfaces/IRegistry.sol and interfaces/IModule.sol

---
+++ contracts/interfaces/IModule.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.24;
+
+import "../introspection/ERC165.sol";
+
+/**
+ * @title Interface for all FansUnite registry contracts
+ */
+contract IModule is ERC165 {
+
+}
---
+++ contracts/interfaces/IRegistry.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.24;
+
+import "../introspection/ERC165.sol";
+
+/**
+ * @title Interface for all FansUnite registry contracts
+ */
+contract IRegistry is ERC165 {
+
+  /**
+   * @notice get the contract address
+   * @param _nameKey is the key for the contract address mapping
+   */
+  function getAddress(string _nameKey) view public returns(address);
+
+  /**
+   * @notice change the contract address
+   * @param _nameKey is the key for the contract address mapping
+   * @param _newAddress is the new contract address
+   */
+  function changeAddress(string _nameKey, address _newAddress) public;
+
+}
