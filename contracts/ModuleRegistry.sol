6c0d813d7b4d33022159fbc37e86e363e429716b
Manan Mehta <mehtamanan@icloud.com>
2018-07-10 10:24:00 -0700

    add ModuleRegistry stubs

--- contracts/ModuleRegistry.sol
+++ contracts/ModuleRegistry.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.24;
 
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./interfaces/IModuleRegistry.sol";
 
 
 /**
@@ -8,6 +9,6 @@ import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 * @notice Anyone can register Resolver modules, but only those "approved" by FansUnite will be available for leagues to add
 * @notice BetValidator modules can only be added by Fansunite
 */
-contract ModuleRegistry is Ownable {
+contract ModuleRegistry is Ownable, IModuleRegistry {
 
 }
---
+++ contracts/interfaces/IModuleRegistry.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.24;
+
+
+/**
+* @title Interface for ModuleRegistry Contract
+*/
+contract IModuleRegistry {
+
+}
