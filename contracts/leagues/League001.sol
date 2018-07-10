69b7e58d225cb900bdfe1c5eeadc96451e6a6f96
Manan Mehta <mehtamanan@icloud.com>
2018-07-09 17:29:52 -0700

    add stubs in contracts/leagues

---
+++ contracts/leagues/League001.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "../interfaces/ILeague.sol";
+import "../introspection/ERC165.sol";
+
+
+/**
+* @title League Contract
+*/
+contract League is ERC165, ILeague {
+
+}
---
+++ contracts/leagues/LeagueFactory001.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.24;
+
+import "../introspection/ERC165.sol";
+import "../interfaces/ILeagueFactory.sol";
+
+
+/**
+* @title League Contract
+*/
+contract League is ERC165, ILeagueFactory {
+
+}
