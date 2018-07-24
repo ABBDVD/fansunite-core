edee7b97358b7e9c22195e9cfc8cd2e8d26079b4
Manan Mehta <mehtamanan@icloud.com>
2018-07-24 11:32:44 -0700

    define IResolver

--- contracts/interfaces/IResolver.sol
+++ contracts/interfaces/IResolver.sol
@@ -6,4 +6,17 @@ pragma solidity ^0.4.24;
  */
 contract IResolver {
 
+  /**
+   * @notice Gets the signature of the init function
+   * @return The function signature compliant with ABI Specification
+   */
+  function getInitSignature() external view returns (string);
+
+  /**
+   * @notice Gets the selector of the init function
+   * @dev Probably don't need this function as getInitSignature can be used to compute selector
+   * @return Selector for the init function
+   */
+  function getInitSelector() external pure returns (bytes4);
+
 }
