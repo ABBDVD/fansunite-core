240557e1fbe03b2e3e5d3d17fa619ec2dc465b16
Manan Mehta <mehtamanan@icloud.com>
2018-07-26 11:47:46 -0700

    implement RMoneyLine (wip)

--- contracts/resolvers/RMoneyLine.sol
+++ contracts/resolvers/RMoneyLine.sol
@@ -1,27 +1,58 @@
 pragma solidity ^0.4.24;
 
-
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../interfaces/IResolver.sol";
 
 
 /**
  * @title MoneyLine Resolver
- * @dev TODO
+ * @dev TODO pre:Manan - inherit ERC165
  */
-contract RMoneyLine is IResolver {
+contract RMoneyLine is Ownable, IResolver {
 
   // League versions supported by RMoneyLine
-  string[] public versions;
+  mapping(string => bool) internal versions;
+
+  // Emit when resolver is validated to support new league version
+  event LogSupportVersion(string _version);
+
+  /**
+   * @notice Constructor
+   * @param _version Base version Resolver supports
+   */
+  constructor(string _version) public {
+    versions[_version] = true;
+  }
 
   /**
    * @notice Support league version `_version`
-   * @dev TODO: Manan => FansUnite only? Owner only?
-   * @dev TODO: Manan => Emit event
    * @param _version League version
    */
-  function supportVersion(string _version) external {
-    versions.push(_version);
+  function supportVersion(string _version) external onlyOwner {
+    require(versions[_version] == false, "Resolver supports version already");
+    versions[_version] = true;
+    emit LogSupportVersion(_version);
   }
+
+  /**
+   * @notice Returns the Result of a Moneyline bet
+   * @param _bWinner bet payload encoded winner participant id (backer's pick)
+   * @param _rWinner resolution payload encoded winner participant id (resolution data)
+   * @return 1 if backer loses and 2 if backer wins
+   */
+  function resolve(uint _bWinner, uint _rWinner) external pure returns (uint8) {
+    return _bWinner == _rWinner ? 2 : 1;
+  }
+
+  /**
+   * @notice Check if participant `_winner` is scheduled in fixture `_fixture` in league `_league`
+   * @dev TODO pre:Manan => Finish implementation
+   * @param _league League Address to perform validation for
+   * @param _fixture Id of fixture
+   * @param _winner Id of participant from bet payload
+   * @return True if bet payload valid, false otherwise
+   */
+  function validate(address _league, uint _fixture, uint _winner) external view returns (bool);
 
   /**
    * @notice Gets the signature of the init function
@@ -59,32 +90,11 @@ contract RMoneyLine is IResolver {
 
   /**
    * @notice Checks whether resolver works with a specific league version
-   * @dev TODO: Manan => Finish implementation
    * @param _version League version
    * @return True if resolver supports league version `_version`, false otherwise
    */
-  function doesSupportVersion(string _version) external view returns (bool);
-
-  /**
-   * @notice Returns the Result of a Moneyline bet
-   * @param _bWinner bet payload encoded winner participant id (backer's pick)
-   * @param _rWinner resolution payload encoded winner participant id (resolution data)
-   * @return 1 if backer loses and 2 if backer wins
-   */
-  function resolve(uint _bWinner, uint _rWinner) external pure returns (uint8) {
-    if (_bWinner == _rWinner)
-      return 2;
-    return 1;
+  function doesSupportVersion(string _version) external view returns (bool) {
+    return versions[_version];
   }
-
-  /**
-   * @notice Check if participant `_winner` is scheduled in fixture `_fixture` in league `_league`
-   * @dev TODO: Manan => Finish implementation
-   * @param _league League Address to perform validation for
-   * @param _fixture Id of fixture
-   * @param _winner Id of participant from bet payload
-   * @return True if bet payload valid, false otherwise
-   */
-  function validate(address _league, uint _fixture, uint _winner) external view returns (bool);
 
 }
