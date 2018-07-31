c5bc9acbf1eaec941198e011cd4584b9e9008bbe
Manan Mehta <mehtamanan@icloud.com>
2018-07-30 23:25:04 -0700

    discard ERC165

--- contracts/leagues/League001.sol
+++ contracts/leagues/League001.sol
@@ -1,12 +1,11 @@
 pragma solidity ^0.4.24;
 
 import "./ILeague001.sol";
-import "../introspection/ERC165.sol";
 
 
 /**
  * @title League Contract
  */
-contract League is ERC165, ILeague001 {
+contract League001 is ILeague001 {
 
 }
