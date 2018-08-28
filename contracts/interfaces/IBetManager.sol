c3735631e3f2a7bf4f7c60661180da9a0edcd71e
Manan Mehta <mehtamanan@icloud.com>
2018-08-27 17:24:26 -0700

    manual merge of IVault and IBetManager

--- contracts/interfaces/IBetManager.sol
+++ contracts/interfaces/IBetManager.sol
@@ -2,8 +2,38 @@ pragma solidity ^0.4.24;
 
 
 /**
-* @title Interface for Bet Manger Contract
-*/
+ * @title Interface for Bet Manger Contract
+ */
 contract IBetManager {
 
+  /**
+   * @notice Claims a bet, transfers tokens and fees based on fixture resolution
+   * @param _bet Hash of all the bet parameters
+   * @return Result of bet
+   */
+  function claimBet(bytes32 _bet) external returns (uint8);
+
+  /**
+   * @notice Cancels a bet which has been partially filled
+   * @param _bet Hash of all the bet parameters
+   * @return Returns `true` if bet was successfully cancelled,
+   */
+  function cancelBet(bytes32 _bet) external returns (bool);
+
+  /**
+   * @notice Returns all the bet ids for the provided user address
+   * @param _user Address of the user
+   */
+  function getUserBetIds(address _user) external view returns (bytes32[]);
+
+  /**
+   * @notice Returns all the parameters associated with bet `_bet`
+   * @param _bet Hash of all the bet parameters
+   * @return Addresses associated with `_bet`
+   *  [backer, layer, backerToken, layerToken, feeRecipient, league, resolver]
+   * @return Params associated with bet
+   *  [backerStake, backerFee, layerFee, expiration, fixture, odds]
+   * @return The bet payload
+  */
+  function getBetDetails(bytes32 _bet) external view returns (address[7], uint[6], bytes);
 }
---
+++ contracts/interfaces/IVault.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.24;
+
+/**
+ * @title Interface for the Vault contract
+ */
+contract IVault {
+
+  /**
+   * @notice Deposits a specific token or ETH
+   * @param _token Address of token that is being deposited
+   * @param _amount Number of tokens to deposit
+   */
+  function deposit(address _token, uint _amount) external payable;
+
+  /**
+   * @notice Withdraws a specific token or ETH
+   * @param _token Address of token that is being withdrawn
+   * @param _amount Number of tokens to withdraw
+   */
+  function withdraw(address _token, uint _amount) external;
+
+  /**
+   * @notice Transfers token from one address to another
+   * @param _token Address of token that is being transferred
+   * @param _from Address to which tokens are being transferred from
+   * @param _to Address to which tokens are being transferred to
+   * @param _amount Number of tokens being transferred
+   */
+  function transfer(address _token, address _from, address _to, uint _amount) external;
+
+  /**
+   * @notice Approves address `_spender` to transfer balances of the sender
+   * @param _spender Address of the spender
+   */
+  function approve(address _spender) external;
+
+  /**
+   * @notice Adds address `_spender` to spenders
+   * @param _spender Address of the spender
+   */
+  function addSpender(address _spender) external;
+
+  /**
+   * @notice Returns the balance of a user for a specified token
+   * @param _token Address of the token
+   * @param _user Address of the user
+   * @return token balance for the user
+   */
+  function balanceOf(address _token, address _user) external view returns (uint);
+
+  /**
+   * @notice Checks if address `_spender` has been approved by user `_user`
+   * @param _user Address of the user
+   * @param _spender Address of the spender
+   * @return `true` if spender `_spender` is approved by user `_user`, `false` otherwise
+   */
+  function isApproved(address _user, address _spender) external view returns (bool);
+
+  /**
+   * @notice Checks if address `_spender` is added as spender
+   * @param _spender Address of the spender
+   * @return `true` if spender `_spender` is added as spender, `false` otherwise
+   */
+  function isSpender(address _spender) external view returns (bool);
+
+}
