6c3454e29e2ea51bbea989d082643eea502866fe
Manan Mehta <mehtamanan@icloud.com>
2018-08-29 13:19:34 -0700

    format / clean up .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   "extends": "eslint:recommended",
   "rules": {
-    "indent": ["error", 2],
-    "semi": ["error", "always"],
-    "comma-dangle": ["error", "never"]
+    "indent": [
+      "error",
+      2
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "comma-dangle": [
+      "error",
+      "never"
+    ]
   },
   "globals" : {
     "artifacts": false,
