a030c379c3b58cadc249986b5575566c7487cb08
Manan Mehta <mehtamanan@icloud.com>
2018-09-14 11:16:37 -0700

    fix linting error

--- contracts/interfaces/IVault.sol
+++ contracts/interfaces/IVault.sol
@@ -27,7 +27,9 @@ contract IVault {
    * @param _to Address to which tokens are being transferred to
    * @param _amount Number of tokens being transferred
    */
-  function transferFrom(address _token, address _from, address _to, uint _amount) external returns (bool);
+  function transferFrom(address _token, address _from, address _to, uint _amount)
+    external
+    returns (bool);
 
   /**
    * @notice Approves address `_spender` to transfer balances of the sender
