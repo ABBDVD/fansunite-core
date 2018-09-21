87fe4fa0b9c25733e897cc14d0473c31e4509d1a
Manan Mehta <mehtamanan@icloud.com>
2018-09-21 16:20:38 -0700

    implement ResolverRegistry

--- contracts/interfaces/IResolverRegistry.sol
+++ contracts/interfaces/IResolverRegistry.sol
@@ -23,6 +23,20 @@ contract IResolverRegistry {
   function registerResolver(string _class, address _resolver) external;
 
   /*
+   * @title Rejects pending resolver `_resolver`, only FansUnite can call this
+   * @param _resolver Address of the resolver contract
+   * @param _class Class of league that resolver `_resolver` will be rejected for
+   */
+  function rejectResolver(string _class, address _resolver) external;
+
+  /*
+   * @title Removes registered resolver in `_class` at index `_index`, only FansUnite can call this
+   * @param _index Index of the resolver in array resolvers[_class]
+   * @param _class Class of league that resolver will be removed for
+   */
+  function nukeResolver(string _class, uint _index) external;
+
+  /*
    * @title Only registered leagues call this function when they add new resolvers to league
    * @param _class Class of league
    * @param _resolver Address of the resolver contract
@@ -38,17 +52,10 @@ contract IResolverRegistry {
   function getResolvers(string _class) external view returns (address[]);
 
   /*
-   * @title Gets all resolvers pending to be registered
-   * @param _class Class of the league
-   * @return Addresses of resolvers pending registration for `_class`
-   */
-  function getPendingResolvers(string _class) external view returns (address[]);
-
-  /*
    * @title Returns whether resolver `_resolver` is registered for `_class`
    * @param _class Class of league
    * @param _resolver Address of resolver
-   * @return `0` if not in registry, `1` if pending, `2` if registered
+   * @return `0` if rejected or not in registry, `1` if pending, `2` if registered
    */
   function isResolverRegistered(string _class, address _resolver) external view returns (uint8);
 }
