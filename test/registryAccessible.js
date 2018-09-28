0b73878b7dac8f1b3feff79a56e61a1f0015c533
rcimoszko <ryan.cimoszko@gmail.com>
2018-09-27 17:28:47 -0700

    added chainspecifiable tests

---
+++ test/registryAccessible.js
@@ -0,0 +1,10 @@
+/* global assert, contract, it, before, afterEach, after, artifacts, describe */
+
+let RegistryAccessible = artifacts.require('./utils/RegistryAccessible')
+  , { ensureException } = require('./helpers/utils');
+
+contract('RegistryAccessible', async accounts => {
+
+
+
+});
